8fa70a3dfa14d86d439e07f896c9624c169e78dd
dpr7 <41078924+DPR7@users.noreply.github.com>
2021-12-18 09:34:56 +0100

    Create #8-3-2 single-post-cta.php

---
+++ #8-3-2 single-post-cta.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       single-post-cta
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Adds sidebar to single posts
+ * Version:           0.1
+ * Author:            Tom van Loon
+ * Author URI:        https://dprdesign.nl
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       spc
+ */
+
+if ( !defined( 'ABSPATH' ) ) {
+	die;
+}
+
+?>
